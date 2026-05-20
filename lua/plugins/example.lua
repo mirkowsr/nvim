24ba0c8b0705ac13edd12c8ad3a06153f25f5f52
rafal mirkowski <rafal.mirkowski@allegro.com>
2026-05-20 21:08:03 +0200

    update config

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -9,14 +9,14 @@ if true then return {} end
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  -- add kanagawa
+  { "rebelot/kanagawa.nvim" },
 
-  -- Configure LazyVim to load gruvbox
+  -- Configure LazyVim to load kanagawa
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "kanagawa",
     },
   },
 
